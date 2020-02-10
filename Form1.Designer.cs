6637dbc390927f0ac75da1713b4c97ee83159478
Christos Katsenos <ckat@intracom-telecom.com>
2020-02-10 15:10:38 +0200

    Adding Open dialog

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace IconsGenerator
 {
-    partial class Form1
+    partial class IconsGenerator
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,196 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnSelectIcon = new System.Windows.Forms.Button();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.lblIconsPath = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblGenIconsPath = new System.Windows.Forms.Label();
+            this.checkBoxiPhone = new System.Windows.Forms.CheckBox();
+            this.checkBoxiPad = new System.Windows.Forms.CheckBox();
+            this.checkBoxWatch = new System.Windows.Forms.CheckBox();
+            this.pictureBoxIcon = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBoxMac = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBoxAndroid = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSelectIcon
+            // 
+            this.btnSelectIcon.Location = new System.Drawing.Point(12, 12);
+            this.btnSelectIcon.Name = "btnSelectIcon";
+            this.btnSelectIcon.Size = new System.Drawing.Size(92, 38);
+            this.btnSelectIcon.TabIndex = 0;
+            this.btnSelectIcon.Text = "Select icon";
+            this.btnSelectIcon.UseVisualStyleBackColor = true;
+            this.btnSelectIcon.Click += new System.EventHandler(this.btnSelectIcon_Click);
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnGenerate.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnGenerate.Location = new System.Drawing.Point(12, 490);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(621, 52);
+            this.btnGenerate.TabIndex = 1;
+            this.btnGenerate.Text = "Generate icons";
+            this.btnGenerate.UseVisualStyleBackColor = false;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // lblIconsPath
+            // 
+            this.lblIconsPath.AutoSize = true;
+            this.lblIconsPath.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblIconsPath.Location = new System.Drawing.Point(110, 23);
+            this.lblIconsPath.Name = "lblIconsPath";
+            this.lblIconsPath.Size = new System.Drawing.Size(85, 17);
+            this.lblIconsPath.TabIndex = 2;
+            this.lblIconsPath.Text = "Icons path...";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Generated icons path:";
+            // 
+            // lblGenIconsPath
+            // 
+            this.lblGenIconsPath.AutoSize = true;
+            this.lblGenIconsPath.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblGenIconsPath.Location = new System.Drawing.Point(167, 62);
+            this.lblGenIconsPath.Name = "lblGenIconsPath";
+            this.lblGenIconsPath.Size = new System.Drawing.Size(20, 17);
+            this.lblGenIconsPath.TabIndex = 4;
+            this.lblGenIconsPath.Text = "...";
+            // 
+            // checkBoxiPhone
+            // 
+            this.checkBoxiPhone.AutoSize = true;
+            this.checkBoxiPhone.Location = new System.Drawing.Point(15, 134);
+            this.checkBoxiPhone.Name = "checkBoxiPhone";
+            this.checkBoxiPhone.Size = new System.Drawing.Size(74, 21);
+            this.checkBoxiPhone.TabIndex = 5;
+            this.checkBoxiPhone.Text = "iPhone";
+            this.checkBoxiPhone.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxiPad
+            // 
+            this.checkBoxiPad.AutoSize = true;
+            this.checkBoxiPad.Location = new System.Drawing.Point(15, 161);
+            this.checkBoxiPad.Name = "checkBoxiPad";
+            this.checkBoxiPad.Size = new System.Drawing.Size(58, 21);
+            this.checkBoxiPad.TabIndex = 6;
+            this.checkBoxiPad.Text = "iPad";
+            this.checkBoxiPad.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxWatch
+            // 
+            this.checkBoxWatch.AutoSize = true;
+            this.checkBoxWatch.Location = new System.Drawing.Point(15, 188);
+            this.checkBoxWatch.Name = "checkBoxWatch";
+            this.checkBoxWatch.Size = new System.Drawing.Size(70, 21);
+            this.checkBoxWatch.TabIndex = 7;
+            this.checkBoxWatch.Text = "Watch";
+            this.checkBoxWatch.UseVisualStyleBackColor = true;
+            // 
+            // pictureBoxIcon
+            // 
+            this.pictureBoxIcon.Location = new System.Drawing.Point(253, 93);
+            this.pictureBoxIcon.Name = "pictureBoxIcon";
+            this.pictureBoxIcon.Size = new System.Drawing.Size(380, 380);
+            this.pictureBoxIcon.TabIndex = 8;
+            this.pictureBoxIcon.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Location = new System.Drawing.Point(12, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(147, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "--- iOS and macOS ---";
+            // 
+            // checkBoxMac
+            // 
+            this.checkBoxMac.AutoSize = true;
+            this.checkBoxMac.Location = new System.Drawing.Point(15, 215);
+            this.checkBoxMac.Name = "checkBoxMac";
+            this.checkBoxMac.Size = new System.Drawing.Size(56, 21);
+            this.checkBoxMac.TabIndex = 10;
+            this.checkBoxMac.Text = "Mac";
+            this.checkBoxMac.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label3.Location = new System.Drawing.Point(9, 251);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 17);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "--- Android ---";
+            // 
+            // checkBoxAndroid
+            // 
+            this.checkBoxAndroid.AutoSize = true;
+            this.checkBoxAndroid.Location = new System.Drawing.Point(12, 277);
+            this.checkBoxAndroid.Name = "checkBoxAndroid";
+            this.checkBoxAndroid.Size = new System.Drawing.Size(79, 21);
+            this.checkBoxAndroid.TabIndex = 11;
+            this.checkBoxAndroid.Text = "Android";
+            this.checkBoxAndroid.UseVisualStyleBackColor = true;
+            // 
+            // IconsGenerator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(645, 554);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.checkBoxAndroid);
+            this.Controls.Add(this.checkBoxMac);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBoxIcon);
+            this.Controls.Add(this.checkBoxWatch);
+            this.Controls.Add(this.checkBoxiPad);
+            this.Controls.Add(this.checkBoxiPhone);
+            this.Controls.Add(this.lblGenIconsPath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblIconsPath);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.btnSelectIcon);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "IconsGenerator";
+            this.Text = "IconsGenerator";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIcon)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSelectIcon;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label lblIconsPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblGenIconsPath;
+        private System.Windows.Forms.CheckBox checkBoxiPhone;
+        private System.Windows.Forms.CheckBox checkBoxiPad;
+        private System.Windows.Forms.CheckBox checkBoxWatch;
+        private System.Windows.Forms.PictureBox pictureBoxIcon;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBoxMac;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBoxAndroid;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,52 @@ using System.Windows.Forms;
 
 namespace IconsGenerator
 {
-    public partial class Form1 : Form
+    public partial class IconsGenerator : Form
     {
-        public Form1()
+        public IconsGenerator()
         {
             InitializeComponent();
         }
+
+        private void btnSelectIcon_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+            var fileName = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                //openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "icon files (*.ico)|*.ico|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+                openFileDialog.Multiselect = false;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+                    fileName = openFileDialog.SafeFileName;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                    }
+                }
+            }
+            lblIconsPath.Text = filePath;
+            lblGenIconsPath.Text = filePath.Replace(fileName,"");
+            //pictureBoxIcon.Image = Bitmap.FromHicon(new Icon(filePath, new Size(380, 380)).Handle);
+
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
